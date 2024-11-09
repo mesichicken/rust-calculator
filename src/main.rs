29aa7f487cd00fc756b8904b970453d20a7dd798
mesi <mesicharida202@gmail.com>
2024-11-09 19:27:14 +0900

    Refactor calculator logic to include memory functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memory: f64 = 0.0;
+    let mut memories: Vec<f64> = vec![0.0; 10];
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         let line = line.unwrap();
@@ -12,18 +12,17 @@ fn main() {
         // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
-        if tokens[0] == "mem+" {
-            add_and_print_memory(&mut memory, prev_result);
+        let is_memory = tokens[0].starts_with("mem");
+        if is_memory && tokens[0].ends_with('+') {
+            add_and_print_memory(&mut memories, tokens[0], prev_result);
             continue;
-        } else if tokens[0] == "mem-" {
-            add_and_print_memory(&mut memory, -prev_result);
+        } else if is_memory && tokens[0].ends_with('-') {
+            add_and_print_memory(&mut memories, tokens[0], -prev_result);
             continue;
         }
 
-        let left: f64 = eval_token(tokens[0], memory);
-
-        let right: f64 = eval_token(tokens[2], memory);
-
+        let left: f64 = eval_token(tokens[0], &memories);
+        let right: f64 = eval_token(tokens[2], &memories);
         let result = eval_expression(left, tokens[1], right);
 
         print_output(result);
@@ -31,14 +30,16 @@ fn main() {
     }
 }
 
-fn add_and_print_memory(memory: &mut f64, prev_result: f64) {
-    *memory += prev_result;
-    print_output(*memory);
+fn add_and_print_memory(memories: &mut [f64], token: &str, prev_result: f64) {
+    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
+    memories[slot_index] += prev_result;
+    print_output(memories[slot_index]);
 }
 
-fn eval_token(token: &str, memory: f64) -> f64 {
-    if token == "mem" {
-        memory
+fn eval_token(token: &str, memories: &[f64]) -> f64 {
+    if token.starts_with("mem") {
+        let slot_index: usize = token[3..].parse().unwrap();
+        memories[slot_index]
     } else {
         token.parse().unwrap()
     }
