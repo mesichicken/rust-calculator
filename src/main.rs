6c0acd86b1c5b0ac5593f9bf743e1f4aa6390c07
mesi <mesicharida202@gmail.com>
2024-11-09 18:16:40 +0900

    Refactor calculator logic to include memory functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::stdin;
 
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         let line = line.unwrap();
         if line.is_empty() {
@@ -10,19 +12,44 @@ fn main() {
         // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[2].parse().unwrap();
-        let result = match tokens[1] {
-            "+" => left + right,
-            "-" => left - right,
-            "*" => left * right,
-            "/" => left / right,
-            _ => {
-                unreachable!()
-            }
-        };
+        if tokens[0] == "mem+" {
+            memory += prev_result;
+            print_output(memory);
+            continue;
+        } else if tokens[0] == "mem-" {
+            memory -= prev_result;
+            print_output(memory);
+            continue;
+        }
 
-        print_output(result)
+        let left: f64 = eval_token(tokens[0], memory);
+
+        let right: f64 = eval_token(tokens[2], memory);
+
+        let result = eval_expression(left, tokens[1], right);
+
+        print_output(result);
+        prev_result = result;
+    }
+}
+
+fn eval_token(token: &str, memory: f64) -> f64 {
+    if token == "mem" {
+        memory
+    } else {
+        token.parse().unwrap()
+    }
+}
+
+fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
+    match operator {
+        "+" => left + right,
+        "-" => left - right,
+        "*" => left * right,
+        "/" => left / right,
+        _ => {
+            unreachable!()
+        }
     }
 }
 
