52ebed13584789d073951ee79ca45a8509caaf34
mesi <mesicharida202@gmail.com>
2024-11-09 22:35:57 +0900

    Refactor calculator logic to include memory functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memories: Vec<f64> = vec![0.0; 10];
+    let mut memory = Memory {
+        slots: vec![0.0; 10],
+    };
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         let line = line.unwrap();
@@ -14,15 +16,15 @@ fn main() {
 
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with('+') {
-            add_and_print_memory(&mut memories, tokens[0], prev_result);
+            add_and_print_memory(&mut memory, tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with('-') {
-            add_and_print_memory(&mut memories, tokens[0], -prev_result);
+            add_and_print_memory(&mut memory, tokens[0], -prev_result);
             continue;
         }
 
-        let left: f64 = eval_token(tokens[0], &memories);
-        let right: f64 = eval_token(tokens[2], &memories);
+        let left: f64 = eval_token(tokens[0], &memory);
+        let right: f64 = eval_token(tokens[2], &memory);
         let result = eval_expression(left, tokens[1], right);
 
         print_output(result);
@@ -30,16 +32,16 @@ fn main() {
     }
 }
 
-fn add_and_print_memory(memories: &mut [f64], token: &str, prev_result: f64) {
+fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
     let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memories[slot_index] += prev_result;
-    print_output(memories[slot_index]);
+    memory.slots[slot_index] += prev_result;
+    print_output(memory.slots[slot_index]);
 }
 
-fn eval_token(token: &str, memories: &[f64]) -> f64 {
+fn eval_token(token: &str, memory: &Memory) -> f64 {
     if token.starts_with("mem") {
         let slot_index: usize = token[3..].parse().unwrap();
-        memories[slot_index]
+        memory.slots[slot_index]
     } else {
         token.parse().unwrap()
     }
@@ -59,4 +61,8 @@ fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
 
 fn print_output(value: f64) {
     println!("The value is {}", value)
+}
+
+struct Memory {
+    slots: Vec<f64>,
 }
